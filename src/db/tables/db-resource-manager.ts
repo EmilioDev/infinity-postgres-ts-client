58bd92c8bd7b05daf01c5955bfcc6ab21d522bbe
Bismarck <emiliocardero1@gmail.com>
2023-12-20 16:23:02 -0500

    added resource table manager

---
+++ src/db/tables/db-resource-manager.ts
@@ -0,0 +1,59 @@
+import { PrismaClient } from "@prisma/client";
+import { 
+    ResourceSelectionableFields,
+    ResourceCreateData,
+    ResourceIdentifierNotObligatory,
+    ResourceIdentifierObligatory 
+} from "../../types";
+
+
+export class DBResourcesManager
+{
+    constructor(private prisma:PrismaClient){
+        // :-)
+    }
+
+    private get data() {
+        return this.prisma.resource;
+    }
+
+    create(data: ResourceCreateData) {
+        return this.data.create({ data });
+    }
+
+    findFirst({ where, select } : {
+        where: ResourceIdentifierNotObligatory, select?: ResourceSelectionableFields
+    })
+    {
+        return this.data.findFirst({
+            where,
+            select
+        });
+    }
+
+    findUnique({ where, select } : {
+        where: ResourceIdentifierObligatory, select?: ResourceSelectionableFields
+    })
+    {
+        return this.data.findUnique({
+            where,
+            select
+        });
+    }
+
+    findAll(where?:ResourceIdentifierNotObligatory) {
+        return this.data.findMany({
+            where
+        });
+    }
+
+    delete({ where, select } : {
+        where: ResourceIdentifierObligatory,
+        select?: ResourceSelectionableFields
+    }) {
+        return this.data.delete({
+            where,
+            select
+        });
+    }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from './user.types';
 export * from './institution.types';
 export * from './metodologist.types';
 export * from './subject.types';
+export * from './resource.types';
